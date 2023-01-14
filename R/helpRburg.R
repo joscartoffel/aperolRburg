58c468464d9a362ac49d0b48818a189e66707b17
joscartoffel <joscha.franke@gmail.com>
2023-01-14 11:07:21 +0100

    add help function

---
+++ R/helpRburg.R
@@ -0,0 +1,21 @@
+#' @title helpRburg
+#'
+#' @description Start in aperolRburg
+#'
+#' @param
+#'
+#' @return Kurzanleitung.
+#'
+#' @export
+#'
+#'
+
+helpRburg <- function() {
+  cat(paste0("This R-package acutally can not do much on its own. And it does not have that many functions. But..."))
+  cat(paste0("\nMaybe it can help you figure out what to do in the sometimes stressfull Ausbildung in Marburg. Tell you to keep in mind to also enjoy life while you are at it."))
+  cat(paste0("\nSo lets get started! \\o/"))
+  cat(paste0("\nRight now there are 5 functions only :( Who knows if there will be more in the future."))
+  cat(paste0("\nThey are the following: \n - aperol() \n - gintonic() \n - kaffee() \n - negorni() \n - wassollichtun()"))
+  cat(paste0("\nSo find out what it can do, e.g. by starting with ?aperol or the second of Kants three iconic questions: wassollichtun() \nSee you around :)"))
+
+}
