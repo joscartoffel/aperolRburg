7b28134c430f0a7f06dd9d62d013c83b93fd51b9
joscartoffel <joscha.franke@gmail.com>
2023-06-14 15:51:03 +0200

    Add Sektsorbet. Minor bugfixes.

---
+++ R/sektsorbet.R
@@ -0,0 +1,23 @@
+#' @title sektsorbet
+#'
+#' @description Sekt und Sorbet, so einfach, so gut, so sommerlich. Go for it!
+#'
+#' @param
+#'
+#' @return Wie du Sekt-Sorbet machen kannst.
+#'
+#' @export
+#'
+#'
+
+sektsorbet <- function() {
+  cat(paste0("Gibt es etwas zu feiern? Oder ist etwas Schaumwein übrig geblieben von einer vortäglichen Feier?"))
+  cat(paste0("\nOder Du gehst extra einkaufen, es lohnt sich!\n"))
+  cat(paste0("\nWir brauchen nicht viel:\n Eine nicht zu süße Flasche Sekt (kalt!). \n Und Zitronensorbet.\n Optional einen Eislöffel.\n"))
+  cat(paste0("\nIn ein Sektglas 1 bis 2 Kugeln Zitronensorbet geben."))
+  cat(paste0("\nMit Sekt auffüllen und nach Gusto mit z.B. Zitronenmelisse dekorieren."))
+  cat(paste0("\nWichtig: Keinen Stress, schemckt genau so gut ohne Deko.\n"))
+  cat(paste0("\n*Best enjoyed on The Bänk.*"))
+
+}
+
--- R/wassollichtun.R
+++ R/wassollichtun.R
@@ -4,14 +4,14 @@
 #'
 #' @param
 #'
-#' @return Ist nur ein Vorschla, make up your own mind ;)
+#' @return Ist nur ein Vorschlag, make up your own mind ;)
 #'
 #' @export
 #'
 #'
 
 wassollichtun <- function() {
-  x <- sample(1:10, 1, replace = TRUE)
+  x <- sample(1:11, 1, replace = TRUE)
   if(x == 1){aperol()}
   if(x == 2){gintonic()}
   if(x == 3){kaffee()}
@@ -22,5 +22,6 @@ wassollichtun <- function() {
   if(x == 8){paste0("Mach doch mal 5 Minuten die Augen zu und denk an etwas schönes.")}
   if(x == 9){paste0("Urlaub, was sonst?")}
   if(x == 10){paste0("Schreib doch einem lieben Menschen eine kurze Nachricht.")}
+  if(x == 11){sektsorbet()}
 
 }
