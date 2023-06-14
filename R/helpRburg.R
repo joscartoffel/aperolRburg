ad8ce594154ff1cd0856467599ecdee966d52567
joscartoffel <122533195+joscartoffel@users.noreply.github.com>
2023-06-14 15:53:55 +0200

    Update README.md

--- R/helpRburg.R
+++ R/helpRburg.R
@@ -14,7 +14,7 @@ helpRburg <- function() {
   cat(paste0("This R-package acutally can not do much on its own. And it does not have that many functions. But..."))
   cat(paste0("\nMaybe it can help you figure out what to do in the sometimes stressfull Ausbildung in Marburg. Tell you to keep in mind to also enjoy life while you are at it."))
   cat(paste0("\nSo lets get started! \\o/"))
-  cat(paste0("\nRight now there are 5 functions only :( Who knows if there will be more in the future."))
+  cat(paste0("\nRight now there are 7 functions only :( Who knows if there will be more in the future."))
   cat(paste0("\nThey are the following: \n - aperol() \n - gintonic() \n - kaffee() \n - negroni() \n - wassollichtun()"))
   cat(paste0("\nSo find out what it can do, e.g. by starting with ?aperol or the second of Kants three iconic questions: wassollichtun() \nSee you around :)"))
 
